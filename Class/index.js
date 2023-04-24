7cbdbb953b87c22009d6e44104d1b18fbdcb17d5
alifsakib <alifsakib@gmail.com>
2023-04-24 12:22:33 +0600

    prototype created for Car contructor

--- Class/index.js
+++ Class/index.js
@@ -5,7 +5,15 @@ const Car = function (brand, price, stock) {
 };
 
 // New empty object {} is created
+// Function is called and this = {}
+// enpty objeect is linkd to prototype
+// object is returned
 
 const m8 = new Car("BMW", 100000, 5);
 
-console.log(m8);
+Car.prototype.totalProfit = function () {
+  return this.price * this.stock;
+};
+
+console.log(m8.totalProfit(10 * 5));
+console.log(m8 instanceof Car);
